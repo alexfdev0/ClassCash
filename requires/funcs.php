a1d974dd763fd475fe2c568ee49db5d9d2af0c87
alexthefemboy0 <70788432+alexthefemboy0@users.noreply.github.com>
2024-08-17 15:33:28 -0400

    Create funcs.php

---
+++ requires/funcs.php
@@ -0,0 +1,20 @@
+<?php
+function check_login($con) {
+  if (isset($_SESSION['customerid'])) {
+    $id = $_SESSION['customerid'];
+    $query = "select * from accounts where customerid = '$id' limit 1";
+    $result = mysqli_query($con, $query);
+    if ($result && mysqli_num_rows($result) > 0) {
+      $user_data = mysqli_fetch_assoc($result);
+      if ($user_data['isbanned'] == 1) {
+        header("Location: account_suspended.php");
+        die;
+      }
+      $_SESSION['isloggedin'] = true;
+      return $user_data;
+    }
+  }else{
+    header("Location: http://fayettevillegreen.com/home.php");
+  }
+}
+?>
