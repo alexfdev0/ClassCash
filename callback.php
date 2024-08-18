86b84f0454bdc934720dc8d7fcff76fccd6f22a1
Alexander Flax <alexflax772@gmail.com>
2024-08-18 14:58:37 -0400

    err handling

--- callback.php
+++ callback.php
@@ -11,42 +11,46 @@ $client->addScope("email");
 $client->addScope("profile");
 
 if (isset($_GET['code'])) {
-    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-    $client->setAccessToken($token['access_token']);
+    try {
+        $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+        $client->setAccessToken($token['access_token']);
 
-    $oauth = new Google_Service_Oauth2($client);
-    $userInfo = $oauth->userinfo->get();
+        $oauth = new Google_Service_Oauth2($client);
+        $userInfo = $oauth->userinfo->get();
 
-    $firstName = $userInfo->givenName;
-    $lastName = $userInfo->familyName;
-    $email = $userInfo->email;
+        $firstName = $userInfo->givenName;
+        $lastName = $userInfo->familyName;
+        $email = $userInfo->email;
 
-    $query = "select * from accounts where email='$email' limit 1";
-    $result = mysqli_query($con, $query);
-    if ($result) {
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                $id = $row['id'];
-                $_SESSION['id'] = $id;
-                header("Location: index.php");
-            }
-        } else {
-            $query = "insert into accounts (firstname, lastname, email) values ('$firstName', '$lastName', '$email')";
-            $result = mysqli_query($con, $query);
+        $query = "select * from accounts where email='$email' limit 1";
+        $result = mysqli_query($con, $query);
+        if ($result) {
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $id = $row['id'];
+                    $_SESSION['id'] = $id;
+                    header("Location: index.php");
+                }
+            } else {
+                $query = "insert into accounts (firstname, lastname, email) values ('$firstName', '$lastName', '$email')";
+                $result = mysqli_query($con, $query);
 
-            $query = "select * from accounts where email='$email' limit 1";
-            $result = mysqli_query($con, $query);
-            if ($result) {
-                if (mysqli_num_rows($result) > 0) {
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        $id = $row['id'];
-                        $_SESSION['id'] = $id;
-                        header("Location: index.php");
+                $query = "select * from accounts where email='$email' limit 1";
+                $result = mysqli_query($con, $query);
+                if ($result) {
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $id = $row['id'];
+                            $_SESSION['id'] = $id;
+                            header("Location: index.php");
+                        }
                     }
                 }
             }
         }
-    } 
+    } catch (Exception $e) {
+        echo "Error! " . $e->getMessage();
+    }
 } else {
     echo "Unauthorized. Looping you back now.";
     $authUrl = $client->createAuthUrl();
