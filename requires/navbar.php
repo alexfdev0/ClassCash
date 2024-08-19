ee5cc4b6ba0c95dee5d18d56a0c7f3bb2fb1ce61
alexthefemboy0 <70788432+alexthefemboy0@users.noreply.github.com>
2024-08-19 00:42:31 -0400

    Update navbar.php

--- requires/navbar.php
+++ requires/navbar.php
@@ -4,20 +4,20 @@ echo "
 <nav class='navbar navbar-expand-lg navbar-light bg-light'>
             <div class='container-fluid'>
                 <a class='navbar-brand' href='home_student.php'>ClassCash</a>
-                <div class='navbar-brand'><?php echo $classname; ?></div>
+                <div class='navbar-brand'>" . $classname . "</div>
                 <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
                     <span class='navbar-toggler-icon'></span>
                 </button>
                 <div class='collapse navbar-collapse' id='navbarSupportedContent'>
                     <ul class='navbar-nav me-auto mb-2 mb-lg-0'>
                         <li class='nav-item'>
-                            <a href=<?php echo $clink; ?> class='nav-link'><span class='material-symbols-outlined'>account_circle</span> Class Profile</a>
+                            <a href='" . $clink . "' class='nav-link'><span class='material-symbols-outlined'>account_circle</span> Class Profile</a>
                         </li>
                         <li class='nav-item'>
-                            <a href=<?php echo $slink; ?> class='nav-link'><span class='material-symbols-outlined'>store</span> Shop</a>
+                            <a href='" . $slink . "' class='nav-link'><span class='material-symbols-outlined'>store</span> Shop</a>
                         </li>
                         <li class='nav-item'>
-                            <a href=<?php echo $tlink; ?> class='nav-link'><span class='material-symbols-outlined'>currency_exchange</span> Transfer ClassCoins</a>
+                            <a href='" . $tlink . "' class='nav-link'><span class='material-symbols-outlined'>currency_exchange</span> Transfer ClassCoins</a>
                         </li>
                         <li class='nav-item dropdown'>
                             <a class='nav-link dropdown-toggle' href='#' id='navbarDropdown' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
