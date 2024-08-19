39d24da5fe956abbd10c9bde1e5cebb82601809a
Alexander Flax <alexflax772@gmail.com>
2024-08-18 21:09:34 -0400

    dumb

--- class_overview.php
+++ class_overview.php
@@ -22,7 +22,7 @@ if ($result) {
 }
 
 $query2 = "select * from classes where id='$classid'";
-$result2 = mysqli_query($con, $query);
+$result2 = mysqli_query($con, $query2);
 if ($result2) {
     if (mysqli_num_rows($result2) > 0) {
         while ($row = mysqli_fetch_assoc($result2)) {
