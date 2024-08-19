3e1de6e72b04cc4e0a132d5a1fe52fcbdff77ee0
Alexander Flax <70788432+alexthefemboy0@users.noreply.github.com>
2024-08-19 19:09:32 -0400

    app state

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+$APP_OPEN = false;
 session_start();
 require 'requires/autoload.php';
+if ($APP_OPEN == false) {
+    header("Location: index.html")
+}
 $user_data = check_login($con);
 
 if ($user_data['educator'] == 0) {
