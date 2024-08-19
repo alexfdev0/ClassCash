350eeb2b3be28b2b6a76806b408ec117b3c4e666
Alexander Flax <70788432+alexthefemboy0@users.noreply.github.com>
2024-08-19 19:09:54 -0400

    fix

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $APP_OPEN = false;
 session_start();
 require 'requires/autoload.php';
 if ($APP_OPEN == false) {
-    header("Location: index.html")
+    header("Location: index.html");
 }
 $user_data = check_login($con);
 
