7a93e6d9cb01c766c97c5eb323e32b5271cb6988
Alexander Flax <70788432+alexthefemboy0@users.noreply.github.com>
2024-08-20 17:29:48 -0400

    rmv home

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
-$APP_OPEN = false;
 session_start();
 require 'requires/autoload.php';
-if ($APP_OPEN == false) {
-    header("Location: index.html");
-    exit;
-}
 $user_data = check_login($con);
 
 if ($user_data['educator'] == 0) {
