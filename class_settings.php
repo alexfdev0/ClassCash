44b0284ec127c5d93e0f12380f0ed0deaaac795e
Alexander Flax <70788432+alexthefemboy0@users.noreply.github.com>
2024-08-20 10:50:19 -0400

    settings menu

---
+++ class_settings.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+require "requires/autoload.php";
+$user_data = check_login($con);
+$sid = $user_data['id'];
+
+if ($user_data['educator'] == 0) {
+    echo "Unauthorized.";
+    exit;
+}
+
+$classid = $_GET['sel'];
+
+$classname = "a";
+$classdescr = "";
+$tfrenabled = 0;
+
+$query2 = "select * from classes where id='$classid'";
+$result2 = mysqli_query($con, $query2);
+if ($result2) {
+    if (mysqli_num_rows($result2) > 0) {
+        while ($row = mysqli_fetch_assoc($result2)) {
+            $classname = $row['name'];
+            $classdescr = $row['descr'];
+            $owner = $row['owner'];
+            $tfrenabled = $row['tfrenabled'];
+            if ($sid != $owner) {
+                echo "Unauthorized.";
+                exit;
+            }
+        }
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <title>ClassCash - Class Overview</title>
+    </head>
+    <style>
+        .profile-picture-container {
+            width: 150px;
+            height: 150px;
+            overflow: hidden;
+            border-radius: 50%;
+        }
+
+        .profile-picture {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            display: block;
+        }
+    </style>
+    <body>
+        <?php
+        require 'requires/navbar_educator.php';
+        ?>
+        <h1>Settings</h1><br><br>
+        <h4>Class Information</h4><br>
+        <form method="post">
+            <input name="class_name" class="form-control" placeholder="Class Name" value=<?php echo $classname; ?>><br><br>
+            <textarea class="form-control" rows="3" placeholder="Class Description" value=<?php echo $classdescr; ?>></textarea><br><br>
+            <h4>Class Settings</h4><br>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="enableTransfers" <?php if ($tfrenabled == 0) { echo "checked"; }?>>
+                <label class="form-check-label" for="enableTransfers">
+                    Allow students to send and recieve ClassCoins from eachother.
+                </label>
+            </div><br><br>
+            <button type="submit" class="btn btn-primary">Save Settings</button>
+    </form>
+    </body>
+</html>
