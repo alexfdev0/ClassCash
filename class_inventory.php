392c07663197dd5b581e4b53605af3982e03ff8e
Alexander Flax <alexflax772@gmail.com>
2024-08-25 12:02:32 -0400

    removed profile picture stylesheets where they were not required

--- class_inventory.php
+++ class_inventory.php
@@ -41,21 +41,6 @@ if ($result2) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <title>ClassCash - Class Overview</title>
     </head>
-    <style>
-        .profile-picture-container {
-            width: 150px;
-            height: 150px;
-            overflow: hidden;
-            border-radius: 50%;
-        }
-
-        .profile-picture {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            display: block;
-        }
-    </style>
     <body>
         <?php
         require 'requires/navbar_educator.php';
--- class_inventory_view.php
+++ class_inventory_view.php
@@ -97,21 +97,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <title>ClassCash - Class Overview</title>
     </head>
-    <style>
-        .profile-picture-container {
-            width: 150px;
-            height: 150px;
-            overflow: hidden;
-            border-radius: 50%;
-        }
-
-        .profile-picture {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            display: block;
-        }
-    </style>
     <body>
         <?php
         require 'requires/navbar_educator.php';
